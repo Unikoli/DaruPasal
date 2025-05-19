3500912795d56d45daac1db51fb9efc0d28cb6b2
Unikoli <uniqueoli16@gmail.com>
2025-05-19 14:41:45 +0545

    winter page modified

--- ecom-frontend/src/components/WinterProductList.jsx
+++ ecom-frontend/src/components/WinterProductList.jsx
@@ -53,7 +53,7 @@ export default function WinterProductList() {
   useEffect(() => {
     const fetchWinterProducts = async () => {
       try {
-        const response = await fetch(`${config.API_URL}/api/category/4/products`); // Category ID 6 for winter
+        const response = await fetch(`${config.API_URL}/api/category/2/products`); // Category ID 6 for winter
         const data = await response.json();
         setProducts(data || []);
       } catch (error) {
